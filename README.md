355a7da43af5a98097c3a6c7135a4f93d25006ab
jojo8356 <polsinelli@gmx.com>
2023-07-28 20:28:49 +0200

    1er commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project
+# Project
+# Project
+# Project
+# Project
+# Project
+# Project
+# Project
+# Project
+# Project
---
+++ verif_save_python_exec.py
@@ -0,0 +1,46 @@
+import subprocess
+import os
+import json
+import time
+
+def get_file_size(file_path):
+    try:
+        file_stats = os.stat(file_path)
+        return file_stats.st_size
+    except FileNotFoundError:
+        print(f"Le fichier '{file_path}' n'existe pas.")
+        return None
+
+def write_json(data, file_path):
+    with open(file_path, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=True, indent=4)
+
+def read_json(file):
+    try:
+        with open(file, "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except:
+        data = {}
+    return data
+
+def check_and_copy_files():
+    while True:
+        data = read_json("data_file.json")
+        command = "find /home/johan/loisirs -type f -name '*.py'"
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+        files = result.stdout.splitlines()
+        times = [round(get_file_size(x)) for x in files]
+        dictio = dict(zip(files, times))
+
+        if not os.path.exists("/home/johan/save"):
+            os.mkdir("/home/johan/save")
+
+        for key, value in dictio.items():
+            if key not in data or value != data[key]:
+                os.system(f"cp {key} /home/johan/save/")
+
+        write_json(dictio, "data_file.json")
+        time.sleep(1)  # Wait for an hour before checking again
+
+if __name__ == "__main__":
+    check_and_copy_files()
